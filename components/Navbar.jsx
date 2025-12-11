c2d5cac48dcb90ceacb510c1d93980681416b43c
Uluc Sungur <usungur@gmail.com>
2025-12-11 13:13:10 +0300

    charts added in to mobil menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -262,22 +262,14 @@ const Navbar = () => {
                         >Add Property</Link
                         >
                     )}
-                    {/* {!session && (
-                        <>
-                            <button
-                                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
-                            >
-                                <i className="fa-brands fa-google mr-2"></i>
-                                <span>Login or Register</span>
-                            </button>
-                            <button
-                                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
-                            >
-                                <i className="fa-brands fa-github mr-2"></i>
-                                <span>Login or Register 1</span>
-                            </button>
-                        </>
-                    )} */}
+                    {session && (
+                        <Link
+                            href="/chart"
+                            className={`${pathname === '/chart' ? 'bg-black' : 'hover:bg-gray-900'} text-white rounded-md px-3 py-2`}
+                        >Charts</Link
+                        >
+                    )}
+
                     {!session && providers && Object.values(providers).map((provider) => (
                         <button
                             key={provider.id}
